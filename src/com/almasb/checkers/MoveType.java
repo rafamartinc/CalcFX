4e7b56b0090f7e78f4addcfac9aab6c5383144e3
rafamartinc <r.martinc@outlook.com>
2016-05-14 15:18:48 +0200

    Repository reduced to CalcFX

--- src/com/almasb/checkers/MoveType.java
+++
@@ -1,8 +0,0 @@
-package com.almasb.checkers;
-
-/**
- * @author Almas Baimagambetov (almaslvl@gmail.com)
- */
-public enum MoveType {
-    NONE, NORMAL, KILL
-}
--- src/com/almasb/tutorial9/SaveData.java
+++
@@ -1,9 +0,0 @@
-package com.almasb.tutorial9;
-
-public class SaveData implements java.io.Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    public String name;
-    public int hp;
-}
